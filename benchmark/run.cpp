9fd5ae88d0e002ae854a3e4499a294eba868f5e8
Tiger <tiger4hire@gmail.com>
2018-11-04 09:13:20 +0000

    Now compare with a) list of unique_ptr (old list) - 247.50 ns per access b) vector unique_ptr  (vec list) - 105.10 ns per access c) vector of variants (alt lits) - 45.83 ns per access d) New container (new list) - 24.14ns per access.
    c + d are type-safe containers.

--- benchmark/run.cpp
+++ benchmark/run.cpp
@@ -3,11 +3,24 @@
 #include "ObjectList.h"
 
 using OldList = std::list<std::unique_ptr<Object>>;
+using VecList = std::vector<std::unique_ptr<Object>>;
 using AltList = std::vector<std::variant<TextLabel, Button>>;
 
 OldList MakeOld(int size)
 {
     OldList retval;
+    for (int i = 0; i < size; ++i)
+        if (rand() & 1)
+            retval.push_back(std::make_unique<TextLabel>(""));
+        else
+            retval.push_back(std::make_unique<Button>());
+
+    return retval;
+}
+
+VecList MakeVec(int size)
+{
+    VecList retval;
     for (int i = 0; i < size; ++i)
         if (rand() & 1)
             retval.push_back(std::make_unique<TextLabel>(""));
@@ -46,12 +59,28 @@ static void BM_OldList(benchmark::State& state)
     {
         state.PauseTiming();
         state.SetComplexityN(state.range(0));
-        OldList oldList = MakeOld(state.range(0));
+        auto list = MakeOld(state.range(0));
         state.ResumeTiming();
-        for (auto& elem : oldList)
+        for (auto& elem : list)
             elem->DoNothing();
+        state.PauseTiming();
     }
 }
+
+static void BM_VecList(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+        state.SetComplexityN(state.range(0));
+        auto list = MakeVec(state.range(0));
+        state.ResumeTiming();
+        for (auto& elem : list)
+            elem->DoNothing();
+        state.PauseTiming();
+    }
+}
+
 static void BM_AltList(benchmark::State& state)
 {
     for (auto _ : state)
@@ -62,6 +91,7 @@ static void BM_AltList(benchmark::State& state)
         state.ResumeTiming();
         for (const auto& obj : altList)
             std::visit([](const auto& v) { v.DoNothing(); }, obj);
+        state.PauseTiming();
     }
 }
 static void BM_NewList(benchmark::State& state)
@@ -73,11 +103,13 @@ static void BM_NewList(benchmark::State& state)
         ObjectList newList = MakeNew(state.range(0));
         state.ResumeTiming();
         newList.visit([count = 0](const auto& v) { v.DoNothing(); });
+        state.PauseTiming();
     }
 }
 // Register the function as a benchmark
 BENCHMARK(BM_OldList)->Arg(100)->Arg(500)->Arg(1000)->Arg(10000)->Arg(100000)->Arg(500000)->Complexity();
 BENCHMARK(BM_AltList)->Arg(100)->Arg(500)->Arg(1000)->Arg(10000)->Arg(100000)->Arg(500000)->Complexity();
+BENCHMARK(BM_VecList)->Arg(100)->Arg(500)->Arg(1000)->Arg(10000)->Arg(100000)->Arg(500000)->Complexity();
 BENCHMARK(BM_NewList)->Arg(100)->Arg(500)->Arg(1000)->Arg(10000)->Arg(100000)->Arg(500000)->Complexity();
 
 BENCHMARK_MAIN();
